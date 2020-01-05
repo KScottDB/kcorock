1783c1c92fd548c92b6c4b64b0f17749cc1d952a
KScott <kscott.db@gmail.com>
2020-01-05 12:18:47 -0800

    theme stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {
     QWidget,
     QLabel,
     QIcon,
+    QPushButton,
     QPixmap,
     FlexLayout
 } = require("@nodegui/nodegui");
@@ -10,21 +11,6 @@ const {
 const {
     readFileSync
 } = require("fs");
-
-// Logo
-const logoLabel = new QLabel();
-logoLabel.setObjectName("logoLabel");
-logoLabel.setText("peepee");
-
-// FormMain
-const mainForm = new QWidget();
-mainForm.setLayout(new FlexLayout());
-mainForm.setGeometry(0, 0, 300, 500);
-
-mainForm.layout.addWidget(logoLabel);
-
-// Protect my precious child from the filthy garbage man
-global.win = mainForm;
 
 global.theme = {};
 
@@ -38,13 +24,49 @@ function ThemeApply() {
         global.logo.width(), global.logo.height()
     );
     logoLabel.setPixmap( global.logo );
+
+    openButton.setText( global.theme.strings.openButton );
+    openLabel. setText( global.theme.strings.openLabel  );
+
     mainForm.setStyleSheet( global.theme.style );
 }
 
 function ThemeLoad(filename) {
     global.theme = JSON.parse( readFileSync(filename) );
+    global.theme.style = readFileSync(global.theme.style);
     ThemeApply();
 }
+
+/////////////// DEFINE MAINWINDOW ///////////////
+
+// Logo
+const logoLabel = new QLabel();
+logoLabel.setObjectName("logoLabel"); // #logoLabel {}
+logoLabel.setText("");
+
+// Open Source Button
+const openButton = new QPushButton();
+openButton.setObjectName("openButton");
+
+// Open Source Label
+const openLabel = new QLabel();
+openLabel.setObjectName("openLabel");
+
+// FormMain
+const mainForm = new QWidget();
+mainForm.setObjectName("mainForm"); // #mainForm {}
+mainForm.setLayout(new FlexLayout());
+mainForm.setFixedSize(300, 500);
+
+mainForm.layout.addWidget(logoLabel);
+
+mainForm.layout.addWidget(openLabel);
+mainForm.layout.addWidget(openButton);
+
+// Protect my precious child from the filthy garbage man
+global.win = mainForm;
+
+///////////////                   ///////////////
 
 // Apply themes
 ThemeLoad("assets/theme.json");
